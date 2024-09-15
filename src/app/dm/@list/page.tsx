cb25a040334b51f257142795da53800aa509151b
demonflowne <ibuemmanuel60@gmail.com>
2024-09-16 00:28:22 +0100

    refactored

--- src/app/dm/@list/page.tsx
+++ src/app/dm/@list/page.tsx
@@ -1,52 +1,11 @@
 "use client";
 import ListEmail from "@/components/List/ListEmail";
 import SearchBar from "@/components/List/SearchBar";
-import { useChat, useEmailState } from "@/store";
-import { type Chat } from "@/types/chatType";
-import { type ListName, type ListNameType } from "@/types/listTypes";
-import React, { type FC, Suspense, useEffect, useState } from "react";
+import { useList } from "@/hook/useList";
+import React, { type FC, Suspense } from "react";
 
 const List: FC<Record<string, never>> = () => {
-  const [listName, setListName] = useState<ListName[]>([]);
-  const { chats } = useChat();
-  const { senderEmail } = useEmailState();
-
-  
-
-  const handleData = (names: Chat[]) => {
-    const emailMap: ListNameType = {};
-
-    names.forEach((item) => {
-      const otherEmail =
-        item.receiverEmail !== senderEmail
-          ? item.receiverEmail
-          : item.senderEmail;
-
-      if (emailMap[otherEmail]) {
-        // Update existing entry
-        emailMap[otherEmail].message = item.message;
-        emailMap[otherEmail].status =
-          item.status ?? emailMap[otherEmail].status;
-        emailMap[otherEmail].senderEmail = item.senderEmail;
-      } else {
-        // Create new entry
-        emailMap[otherEmail] = {
-          email: otherEmail,
-          message: item.message,
-          status: item.status ?? "unknown", // Provide a default value if status is undefined
-          senderEmail: item.senderEmail
-        };
-      }
-    });
-    const unique = Object.values(emailMap) as ListName[];
-    setListName(unique);
-    console.log(unique);
-  };
-
-
-  useEffect(() => {
-    handleData(chats);
-  }, [senderEmail, chats]);
+  const { listName } = useList();
 
   return (
     <div className="bg-[#f0ebec] dark:bg-[#202020] rounded-l-2xl overflow-hidden shadow-lg h-[80vh] w-[360px] p-5">
--- src/components/ChatList/InputBox.tsx
+++ src/components/ChatList/InputBox.tsx
@@ -1,62 +1,24 @@
 "use client";
-import { useSocket } from "@/providers/Socket";
-import { useEmailState } from "@/store";
-import React, { type FormEvent, useEffect, useRef, useState } from "react";
+import React from "react";
 import { MdSend } from "react-icons/md";
 import EmojiSelector from "./EmojiSelector";
-import { emojis } from "@/templates/emoji";
 import ImageChat from "./ImageChat";
 import PreUpload from "./PreUpload";
+import { useInputBoxHook } from "@/hook/useInputBox";
 
 const InputBox = () => {
-  const [text, setText] = useState("");
-  const [displayEmoji, _setDisplayEmoji] = useState<string>(emojis[0]);
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [arrayImages, setArrayImages] = useState<string[]>([]);
-  const { email, senderEmail } = useEmailState();
-  // const [loading, setLoading] = useState(false);
-  const { socket } = useSocket();
-  const emojiStarterRef = useRef<HTMLButtonElement | null>(null);
-
-  const textTypeRef = useRef<HTMLInputElement | null>(null);
-
-  // console.log(arrayImages);
-
-  const submitInput = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    socket?.emit("sentMessage", {
-      chatId: Math.floor(Math.random() * 1000000),
-      message: text,
-      senderEmail: senderEmail,
-      receiverEmail: email,
-      image: arrayImages,
-      status: "sent"
-      // date: "Today 7:45 am",
-      // picture: session?.user?.image,
-    });
-    setText("");
-    setArrayImages([]);
-  };
-
-  useEffect(() => {
-    let activityTimer: NodeJS.Timeout;
-    const handleTyping = () => {
-      socket?.emit("typing", senderEmail);
-      clearTimeout(activityTimer);
-      activityTimer = setTimeout(() => {
-        socket?.emit("stop-typing", senderEmail);
-      }, 2000);
-    };
-
-    textTypeRef?.current?.addEventListener("keypress", handleTyping);
-    return () => {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      textTypeRef?.current?.removeEventListener("keypress", handleTyping);
-      socket?.off("typing");
-      socket?.off("stop-typing");
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  const {
+    displayEmoji,
+    emojiStarterRef,
+    isOpen,
+    setIsOpen,
+    submitInput,
+    text,
+    arrayImages,
+    setArrayImages,
+    setText,
+    textTypeRef
+  } = useInputBoxHook();
 
   return (
     <form className="   p-4 " onSubmit={submitInput}>
