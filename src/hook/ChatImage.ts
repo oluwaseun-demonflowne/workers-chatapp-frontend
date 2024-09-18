a716253d71f325d8a375fac8abf21991df0353e1
demonflowne <ibuemmanuel60@gmail.com>
2024-09-18 05:08:52 +0100

    image can be sent

--- src/hook/ChatImage.ts
+++ src/hook/ChatImage.ts
@@ -1,19 +1,28 @@
-import { useState } from "react";
-import toast from "react-hot-toast";
+import { type Dispatch, type SetStateAction, useState } from "react";
 import { useUploadThing } from "../../utils/uploadthing";
+import { toast } from "sonner";
 
-export const useChatImage = () => {
+export const useChatImage = (
+  setArrayImages: Dispatch<SetStateAction<string[]>>
+) => {
   const [loading, setLoading] = useState(false);
   // const [linkUrl, setLinkUrl] = useState("");
   const { startUpload } = useUploadThing("chatImageUpload", {
     onClientUploadComplete: async (res) => {
-      res;
-    },
-    onUploadError: () => {
-      toast.error("Error Updating file, ReUpload");
+      const uploadedUrls = res.map((item) => item.url);
       setLoading(false);
+      setArrayImages((prev) => [...prev, ...uploadedUrls]);
+      toast.success("Image uploaded", { duration: 2000 });
+    },
+    onUploadError: (e) => {
+      console.log(e);
+      toast.error("Error Updating file, ReUpload", { duration: 2000 });
+      setLoading(false);
+      toast.error("Upload error");
     }
   });
+
+  toast.dismiss();
 
   return {
     loading,
