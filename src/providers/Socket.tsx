ef6c80fb915e02f24dcdf4a384626e7f19d91cbb
demonflowne <ibuemmanuel60@gmail.com>
2024-12-15 03:35:51 +0100

    trigger env

--- src/providers/Socket.tsx
+++ src/providers/Socket.tsx
@@ -27,6 +27,7 @@ export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
   const { data: session } = useSession();
 
   useEffect(() => {
+    
     const socket: Socket = io(process.env.NEXT_PUBLIC_BACKEND_URL!);
     // const socket: Socket = io("http://localhost:5001");
 
